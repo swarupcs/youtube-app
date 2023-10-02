04010fee69b37ea705cc9a6bcf4b3042ee76fd98
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-02 22:32:43 +0530

    API Setup and Context API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
+
+import { AppContext } from "./context/contextAPI";
 import './App.css';
 
 function App() {
   return (
+    <AppContext>
     <div className="text-3xl">
      App
-    </div>
+     </div>
+    </AppContext>
+
   );
 }
 
